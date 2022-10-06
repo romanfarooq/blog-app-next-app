da024607cced0aa733b92a6e7e788562c81c13d7
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-07 00:28:50 +0500

    practiced next js routing

---
+++ pages/blogPost/[slug].js
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+
+export default function Slug() {
+  const router = useRouter();
+  const { slug } = router.query;
+  return <div>Slug: {slug}</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -13,10 +14,18 @@ export default function Home() {
 
       <nav className={styles.navbar}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
 
