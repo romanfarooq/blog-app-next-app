e1e4fd159e7db6c2f35a25b01d1d5c3cd2ed99c0
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-11 04:46:56 +0500

    displaying navbar & footer on every page

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>@2022 Blog -- App &copy; All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -12,23 +12,6 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className={styles.navbar}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
       <main className={styles.main}>
         <h1 className={styles.title}>Blog -- App</h1>
         <div className={styles.imageContainer}>
@@ -57,19 +40,6 @@ const Home = () => {
           </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
