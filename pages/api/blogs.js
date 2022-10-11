19cd153c22d8c88b24e95f95c7c2577328ad46fb
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-11 20:52:44 +0500

    fixed bugs

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs';
 
-export default function Blogs(req, res) {
+export default function handler(req, res) {
   try {
     const blogs = fs.readdirSync('blogdata');
     res.status(200).json(blogs);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "Internal Sever Error" });
   }
 }
--- pages/api/getBlog.js
+++ pages/api/getBlog.js
@@ -1,11 +1,12 @@
+// http://localhost:3000/api/getBlog?slug=how-to-learn-javascript
 import fs from 'fs';
 
-export default function getBlog(req, res) {
+export default function handler(req, res) {
   try {
     const { slug } = req.query;
-    const data = fs.readFileSync(`blogdata/${slug}`, 'utf8');
+    const data = fs.readFileSync(`blogdata/${slug}.json`, 'utf8');
     res.status(200).json(JSON.parse(data));
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "Blog Not Found" });
   }
 }
