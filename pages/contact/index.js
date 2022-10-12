7e764c270995d910fa5cb5aeb04517c86663da83
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-13 00:35:46 +0500

    changed ' to &apos;

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -78,7 +78,7 @@ const Contact = () => {
             required
           />
           <div id="emailHelp" className={styles.formText}>
-            We'll never share your email with anyone else.
+            We&apos;ll never share your email with anyone else.
           </div>
         </div>
         <div className={styles.mb3}>
